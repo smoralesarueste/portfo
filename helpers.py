d016b1e30c6d65ca36557c9a26da251bf291107f
smoralesarueste <smoralesarueste@gmail.com>
2022-07-13 16:31:19 -0400

    minor fix

--- helpers.py
+++ helpers.py
@@ -1,9 +1,13 @@
 import pandas as pd
-from datetime import datetime
+import os
 
-def append_answer(filepath, data_dict): 
-	data_dict["datetime"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-	existing_df = pd.read_csv(filepath)
-	existing_df = existing_df.append(data_dict, ignore_index = True)
-	existing_df.to_csv(path_or_buf = filepath, sep = ",", index = False)
-	return True
+from datetime import datetime
+from pathlib import Path
+
+def append_answer(filepath, data_dict):
+        filepath = str(Path(os.path.realpath(__file__)).parent) + "/" + filepath
+        data_dict["datetime"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        existing_df = pd.read_csv(filepath)
+        existing_df = existing_df.append(data_dict, ignore_index = True)
+        existing_df.to_csv(path_or_buf = filepath, sep = ",", index = False)
+        return True
